4205049a0beb148ebeda56f581b2e55bd1dee3dd
jeffyactive <jeff@reelyactive.com>
2016-12-05 21:55:36 -0500

    Updated to latest version

--- js/beaver.js
+++ js/beaver.js
@@ -4,16 +4,23 @@
  */
 
 
+DISAPPEARANCE_MILLISECONDS = 15000;
+DEFAULT_POLLING_MILLISECONDS = 5000;
+
+
 angular.module('reelyactive.beaver', [])
 
-  .factory('beaver', function beaverFactory() {
+  .factory('beaver', function beaverFactory($http) {
 
     var devices = {};
+    var directories = {};
     var stats = { appearances: 0, displacements: 0, keepalives: 0,
                   disappearances: 0 };
     var eventCallbacks = {};
+    var pollingApiUrl;
 
 
+    // Use the given event to update the status of the corresponding device
     function updateDevice(type, event) {
       if(!isValidEvent) {
         return;
@@ -24,6 +31,10 @@ angular.module('reelyactive.beaver', [])
       }
 
       var deviceId = event.deviceId;
+      if(!devices.hasOwnProperty(deviceId)) {
+        type = 'appearance';
+      }
+
       if(type === 'appearance') { stats.appearances++; }
       if(type === 'displacement') { stats.displacements++; }
       if(type === 'keep-alive') { stats.keepalives++; }
@@ -37,17 +48,17 @@ angular.module('reelyactive.beaver', [])
       }
 
       if(!devices.hasOwnProperty(deviceId)) {
-        devices[deviceId] = event;
+        devices[deviceId] = { event:  event };
       }
       else {
         mergeDeviceEvents(devices[deviceId], event);
       }
 
-
       handleEventCallback(type, event);
     }
 
 
+    // Verify if the given event is valid
     function isValidEvent(event) {
       if(!event) {
         return false;
@@ -60,6 +71,7 @@ angular.module('reelyactive.beaver', [])
     }
 
 
+    // Update the given event to the current format
     function updateLegacyEvent(event) {
       event.deviceId = event.tiraid.identifier.value;
       event.time = new Date(event.tiraid.timestamp);
@@ -69,20 +81,60 @@ angular.module('reelyactive.beaver', [])
     }
 
 
+    // Merge any previous device event with the given one
     function mergeDeviceEvents(device, event) {
-      device.deviceAssociationIds = event.deviceAssociationIds ||
-                                    device.deviceAssociationIds;
-      device.deviceUrl = event.deviceUrl || device.deviceUrl;
-      device.deviceTags = event.deviceTags || device.deviceTags;
-      device.receiverId = event.receiverId;
-      device.receiverUrl = event.receiverUrl;
-      device.receiverTags = event.receiverTags;
-      device.receiverDirectory = event.receiverDirectory;
-      device.rssi = event.rssi;
-      device.rssiType = event.rssiType;
+      device.event.event = event.event;
+      device.event.time = event.time;
+      device.event.deviceAssociationIds = event.deviceAssociationIds ||
+                                          device.event.deviceAssociationIds;
+      device.event.deviceUrl = event.deviceUrl || device.event.deviceUrl;
+      device.event.deviceTags = event.deviceTags || device.event.deviceTags;
+      device.event.receiverId = event.receiverId;
+      device.event.receiverUrl = event.receiverUrl;
+      device.event.receiverTags = event.receiverTags;
+      device.event.receiverDirectory = event.receiverDirectory;
+      device.event.rssi = event.rssi;
+      device.event.rssiType = event.rssiType;
     }
 
 
+    // Update the directories of devices
+    function updateDirectories(event) {
+      var directory = event.receiverDirectory;
+      var deviceId = event.deviceId;
+
+      for(cDirectory in directories) {
+        if((directory === cDirectory) && (event.event !== 'disappearance')) {
+          addReceiver(directory, event);
+          directories[cDirectory].devices[deviceId] = devices[deviceId];
+        }
+        else if(directories[cDirectory].devices.hasOwnProperty(deviceId)) {
+          delete directories[cDirectory].devices[deviceId];
+        }
+      }
+
+      if(!directories.hasOwnProperty(directory)) {
+        directories[directory] = { receivers: {}, devices: {} };
+        addReceiver(directory, event);
+        directories[directory].devices[deviceId] = devices[deviceId];
+      }
+    }
+
+
+    // Add the receiver to the given directory
+    function addReceiver(directory, event) {
+      if(!directories[directory].receivers.hasOwnProperty(event.receiverId)) {
+        directories[directory].receivers[event.receiverId] = {
+          receiverId: event.receiverId,
+          receiverTags: event.receiverTags,
+          receiverDirectory: directory,
+          receiverUrl: event.receiverUrl
+        };
+      }
+    }
+
+
+    // Handle any registered callbacks for the given event type
     function handleEventCallback(type, device) {
       var callback = eventCallbacks[type];
       if(callback) {
@@ -91,6 +143,7 @@ angular.module('reelyactive.beaver', [])
     }
 
 
+    // Register a callback for the given event type
     var setEventCallback = function(event, callback) {
       if(callback && (typeof callback === 'function')) { 
         eventCallbacks[event] = callback;
@@ -98,33 +151,149 @@ angular.module('reelyactive.beaver', [])
     }
 
 
+    // Add the given property to the given device
+    function addDeviceProperty(deviceId, property, value) {
+      if(!devices.hasOwnProperty(deviceId) || !property ||
+         (typeof(property) !== 'string')) {
+        return false;
+      }
+      devices[deviceId][property] = value;
+      return true;
+    }
+
+
+    // Add the given property to the given directory
+    function addDirectoryProperty(directory, property, value) {
+      if(!directories.hasOwnProperty(directory) || !property ||
+         (typeof(property) !== 'string')) {
+        return false;
+      }
+      directories[directory][property] = value;
+      return true;
+    }
+
+
+    // Purge any stale devices as disappearances
+    function purgeDisappearances() {
+      var currentTime = new Date().getTime();
+      for(cDevice in devices) {
+        if((currentTime - devices[cDevice].event.time) >
+           DISAPPEARANCE_MILLISECONDS) {
+          handleEventCallback('disappearance', devices[cDevice].event);
+          delete devices[cDevice];
+          stats.disappearances++;
+        }
+      }
+    }
+
+
+    // Handle incoming socket events by type
     var handleSocketEvents = function(Socket) {
 
       Socket.on('appearance', function(event) {
         updateDevice('appearance', event);
+        updateDirectories(event);
       });
 
       Socket.on('displacement', function(event) {
         updateDevice('displacement', event);
+        updateDirectories(event);
       });
 
       Socket.on('keep-alive', function(event) {
         updateDevice('keep-alive', event);
+        updateDirectories(event);
       });
 
       Socket.on('disappearance', function(event) {
         updateDevice('disappearance', event);
+        updateDirectories(event);
       });
 
       Socket.on('error', function(err, data) {
       });
+
+      setInterval(purgeDisappearances, DISAPPEARANCE_MILLISECONDS);
+    };
+
+
+    // Update the given device from the list of polled devices
+    function updatePolledDevice(deviceId, polledDevices) {
+      var device = polledDevices[deviceId];
+      if(!device.hasOwnProperty('nearest')) {
+        return;
+      }
+      var receiverId = device.nearest[0].device;
+      var receiver = polledDevices[receiverId];
+      var event = {
+        deviceId: deviceId,
+        deviceAssociationIds: device.deviceAssociationIds || [],
+        deviceUrl: device.url,
+        deviceTags: device.tags,
+        receiverId: device.nearest[0].device,
+        receiverUrl: receiver.url,
+        receiverTags: receiver.tags,
+        receiverDirectory: receiver.directory,
+        rssi: device.nearest[0].rssi,
+        time: new Date().getTime()
+      };
+      var type = 'appearance';
+
+      if(devices.hasOwnProperty(deviceId)) {
+        if(devices[deviceId].event.receiverId === event.receiverId) {
+          type = 'keep-alive';
+        }
+        else {
+          type = 'displacement';
+        }
+      }
+      event.event = type;
+
+      updateDevice(type, event);
+      updateDirectories(event);
+    }
+
+
+    // Query the polling API
+    function queryApi() {
+      $http.defaults.headers.common.Accept = 'application/json';
+      $http.get(pollingApiUrl)
+        .success(function(data, status, headers, config) {
+          if(data.hasOwnProperty('devices')) {
+            for(deviceId in data.devices) {
+              updatePolledDevice(deviceId, data.devices);
+            }
+            purgeDisappearances();
+          }
+        })
+        .error(function(data, status, headers, config) {
+          console.log('beaver: GET ' + pollingApiUrl + ' returned status ' +
+                      status);
+        });
+    }
+
+
+    // Initialise polling of API
+    var initPolling = function(url, interval) {
+      if(!url || (typeof url !== 'string')) {
+        return;
+      }
+      interval = interval || DEFAULT_POLLING_MILLISECONDS;
+      pollingApiUrl = url;
+
+      queryApi();
+      setInterval(queryApi, interval);
     };
 
 
     return {
       listen: handleSocketEvents,
+      poll: initPolling,
       on: setEventCallback,
+      addDeviceProperty: addDeviceProperty,
+      addDirectoryProperty: addDirectoryProperty,
       getDevices: function() { return devices; },
+      getDirectories: function() { return directories; },
       getStats: function() { return stats; }
     }
   });
--- js/cormorant.js
+++ js/cormorant.js
@@ -29,12 +29,46 @@ angular.module('reelyactive.cormorant', [])
       return json;
     }
 
+    function getStoryTypes(story) {
+      var types = [];
+
+      if(story && story.hasOwnProperty('@graph') &&
+         story['@graph'] instanceof Array) {
+        for(var cType = 0; cType < story['@graph'].length; cType++) {
+          types.push(story['@graph'][cType]['@type']);
+        }
+      }
+
+      return types;
+    }
+
+    function combine(story1, story2) {
+      var types1 = getStoryTypes(story1);
+      var types2 = getStoryTypes(story2);
+
+      if(types1.length > 0) {
+        var combined = Object.assign({}, story1);
+        for(var cType = 0; cType < types2.length; cType++) {
+          var index = types1.indexOf(types2[cType]);
+          if(index < 0) {
+            combined['@graph'].push(story2['@graph'][cType]);
+          }
+          else {
+            combined['@graph'][index] = story2['@graph'][cType];
+          }
+        }
+        return combined;
+      }
+      return story1;
+    }
+
+
     var get = function(url, callback) {
       if(!url || (typeof url !== 'string')) {
-        return callback(null);
+        return callback(null, null);
       }
       if(stories.hasOwnProperty(url)) {
-        return callback(stories[url]);
+        return callback(stories[url], url);
       }
       $http.defaults.headers.common.Accept = 'application/json, text/plain';
       $http.get(url)
@@ -44,18 +78,34 @@ angular.module('reelyactive.cormorant', [])
               data = extractFromHtml(data);
             case 'object':
               stories[url] = data;
-              callback(data);
+              callback(data, url);
           }
         })
         .error(function(data, status, headers, config) {
           console.log('cormorant: GET ' + url + ' returned status ' + status);
           stories[url] = null;
-          callback(null);
+          callback(null, url);
         });
+    };
+
+    var getCombined = function(url1, url2, id, callback) {
+      get(url1, function(story1) {
+        if(!story1) {
+          return callback(null, id);
+        }
+        get(url2, function(story2) {
+          if(!story2) {
+            return callback(story1, id);
+          }
+          var combinedStory = combine(story1, story2);
+          callback(combinedStory, id);
+        });
+      });
     };
 
     return {
       getStory: get,
+      getCombinedStory: getCombined,
       getStories: function() { return stories; }
     }
   });
